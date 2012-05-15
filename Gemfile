9a4000c1844e40e12ea8ff2d18da91eaa66cf064
Scott Fey <scottfey@gmail.com>
2012-05-14 21:25:05 -0500

    Build basic functionality

--- Gemfile
+++ Gemfile
@@ -13,6 +13,11 @@ gem 'sqlite3'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
+  
+group :development do
+  gem 'thin'
+  gem 'quiet_assets'
+end
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platform => :ruby
--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,13 @@
 PracticeApp::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  
+  get '/users', :controller => 'users', :action => 'index', :as => :users
+  post '/users', :controller => 'users', :action => 'create'
+  
+  get '/users/new', :controller => 'users', :action => 'new', :as => :new_user 
+  get '/users/:id', :controller => 'users', :action => 'show', :as => :user 
+  get '/users/:id/edit', :controller => 'users', :action => 'edit', :as => :edit_user 
+  put '/users/:id', :controller => 'users', :action => 'update'
+  delete '/users/:id', :controller => 'users', :action => 'destroy'
+  
+  
 end
